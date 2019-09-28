bb7ec09e391e07ff1fa9aefe514003734ceca768
gabrielfeba <gabriel_feba@outlook.com>
2019-09-28 15:23:12 -0300

    Adicionar conteúdo para compasso2 e evento
    Evento:
    - Adicionado no botão Enter, dispara a função start, a mesma do botão
    iniciar.
    Conteúdo:
    - Modificação na animação do compasso2 e nova animação para mudar a
    bolinha de cor.
    - Mudança na implementação da função desenharCompasso.

--- js/script.js
+++ js/script.js
@@ -7,14 +7,17 @@ const containerDoSolfejo = document.querySelector("body>.containerDasLinhas");
 const linhaDesenho = document.querySelectorAll(".linhaDesenho");
 const corpoDaPagina = document.querySelector("body");
 
-iniciar.onclick = () => {
-    let tipoDeCompasso = pegarTipoDeCompasso();
-    desenharCompasso(tipoDeCompasso);
-    comecarAnimacao(tipoDeCompasso);
-}
+iniciar.onclick = start;
 
 corpoDaPagina.onkeypress = (event) => {
     if (event.code === "KeyF") voltarParaMenu();
+    if (event.code === "Enter") start();
+}
+
+function start() {
+    let tipoDeCompasso = pegarTipoDeCompasso();
+    desenharCompasso(tipoDeCompasso);
+    comecarAnimacao(tipoDeCompasso);
 }
 
 // returna número 2 para compasso binário, 3 para compasso ternário e 4 para compasso quartenário.
@@ -31,17 +34,34 @@ function pegarTipoDeCompasso() {
 function desenharCompasso(tipoDeCompasso) {
     if(tipoDeCompasso === 4) {
         // desenho quartenário.
+        linhaDesenho[1].style.transform = "translate(0,0) rotate(0deg)";
+        linhaDesenho[0].style.height = "80vh";
         linhaDesenho[1].style.transform = "rotate(90deg)";
+        linhaDesenho[1].style.height = "80vh";
         linhaDesenho[2].style.transform = "translate(-20vh, 20vh) rotate(-45deg)";
         linhaDesenho[2].style.height = "57vh";
         linhaDesenho[3].style.transform = "translate(20vh, -20vh) rotate(-45deg)";
         linhaDesenho[3].style.height = "57vh";
     } else if(tipoDeCompasso === 3) {
         // desenho ternário.
+        linhaDesenho[0].style.transform = "translate(0,0) rotate(0deg)";
+        linhaDesenho[0].style.height = "80vh";
+        linhaDesenho[1].style.transform = "translate(0,0) rotate(0deg)";
+        linhaDesenho[1].style.height = "80vh";
         linhaDesenho[2].style.transform = "translate(20vh, 20vh) rotate(45deg)";
         linhaDesenho[2].style.height = "57vh";
         linhaDesenho[3].style.transform = "translate(20vh, -20vh) rotate(-45deg)";
         linhaDesenho[3].style.height = "57vh";
+    } else if(tipoDeCompasso === 2) {
+        // desenho binário.
+        linhaDesenho[0].style.height = "60vh";
+        linhaDesenho[0].style.transform = "translateY(-10vh)";
+        linhaDesenho[1].style.height = "35.3vh";
+        linhaDesenho[1].style.transform = "translate(17.5vh, 22.5vh) rotate(-81deg)";
+        linhaDesenho[2].style.height = "31.5vh";
+        linhaDesenho[2].style.transform = "translate(23vh, 15.5vh) rotate(-50deg)";
+        linhaDesenho[3].style.height = "47vh";
+        linhaDesenho[3].style.transform = "translate(5.5vh, -17.2vh) rotate(-14deg)";
     }
 }
 
@@ -52,7 +72,10 @@ function comecarAnimacao(tipoDeCompasso) {
     // coloca div do solfejo.
     containerDoSolfejo.style.display = "flex";
     // adiciona animação com a velocidade da batida e o tipo de solfejo.
-    animacaoDiv.style.animation = `2s levare ease-in, ${velocidadeDaBatida}s 2s compasso${tipoDeCompasso} linear infinite`;
+    if(tipoDeCompasso === 2)
+        animacaoDiv.style.animation = `2s levare2 ease-in, ${velocidadeDaBatida}s 2s compasso${tipoDeCompasso} linear infinite, ${velocidadeDaBatida}s 2s compasso${tipoDeCompasso}Color infinite`;
+    else 
+        animacaoDiv.style.animation = `2s levare ease-in, ${velocidadeDaBatida}s 2s compasso${tipoDeCompasso} linear infinite`;
 }
 
 function voltarParaMenu() {
